744c0e30fdad4293f65a9d809e899665b8be752c
Güneş Balcı <gunesbalci66@hotmail.com>
2024-10-29 18:48:50 +0300

    Kategorilerin popülerlerini bulma fonksiyonu bitti

--- Paketler/Kategori.py
+++ Paketler/Kategori.py
@@ -5,13 +5,11 @@ filmler = pd.read_csv(filmlerCSV, usecols=["movieId","genres"])
 
 kategoriler = filmler["genres"].apply(lambda x: x.split("|"))
 
-Belgesel = filmler[kategoriler.apply(lambda x: x.__contains__("Documentary"))]
-Sanat = filmler[kategoriler.apply(lambda x: x.__contains__("Film-Noir"))]
-Fantazi = filmler[kategoriler.apply(lambda x: x.__contains__("Fantasy"))]
-Savaş = filmler[kategoriler.apply(lambda x: x.__contains__("War"))]
-Çocuk = filmler[kategoriler.apply(lambda x: x.__contains__("Children"))]
-Korku = filmler[kategoriler.apply(lambda x: x.__contains__("Horror"))]
-Suç = filmler[kategoriler.apply(lambda x: x.__contains__("Crime"))]
-BilimKurgu = filmler[kategoriler.apply(lambda x: x.__contains__("Sci-Fi"))]
-Gizem = filmler[kategoriler.apply(lambda x: x.__contains__("Mystery"))]
-Gerilim = filmler[kategoriler.apply(lambda x: x.__contains__("Thriller"))]
+def KategorilerinPopülerleri(kurallar, kategori):
+    KategoriFilmleri = filmler[kategoriler.apply(lambda x: x.__contains__(kategori))]["movieId"].apply(lambda x: set([str(x)]))
+    kurallar["antecedents_len"] = kurallar["antecedents"].apply(lambda x: len(x))
+    kurallar = kurallar[kurallar['antecedents_len'] == 1]
+    kurallar = kurallar.loc[:,["antecedents","antecedent support"]]
+    kurallar = kurallar[kurallar["antecedents"].isin(KategoriFilmleri)]
+    kurallar = kurallar.sort_values(by='antecedent support',ascending=False)["antecedents"].apply(lambda x: str(x)).drop_duplicates()
+    return kurallar
