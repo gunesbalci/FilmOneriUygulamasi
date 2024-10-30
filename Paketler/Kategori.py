53b5c9f868bcc73a171d0727559aa6dc82b93471
Güneş Balcı <gunesbalci66@hotmail.com>
2024-10-30 19:37:24 +0300

    türe göre kişiselleştirilmiş öneri tamamlandı

--- Paketler/Kategori.py
+++ Paketler/Kategori.py
@@ -1,15 +1,25 @@
 import pandas as pd
+import Paketler.Dosyalar as dosya
 
-filmlerCSV = "C:/Users/gunes/Desktop/odevprogramlama/yazılımgeliştirme/filmönermeuygulaması/HazırVeriler/filmler.csv"
-filmler = pd.read_csv(filmlerCSV, usecols=["movieId","genres"])
+filmler = pd.read_csv(dosya.filmlerCSV, usecols=["movieId","genres"])
 
 kategoriler = filmler["genres"].apply(lambda x: x.split("|"))
 
-def KategorilerinPopülerleri(kurallar, kategori):
+def KategorilerinPopülerleri(kurallar, kategori, antecedent):
     KategoriFilmleri = filmler[kategoriler.apply(lambda x: x.__contains__(kategori))]["movieId"].apply(lambda x: set([str(x)]))
-    kurallar["antecedents_len"] = kurallar["antecedents"].apply(lambda x: len(x))
-    kurallar = kurallar[kurallar['antecedents_len'] == 1]
-    kurallar = kurallar.loc[:,["antecedents","antecedent support"]]
-    kurallar = kurallar[kurallar["antecedents"].isin(KategoriFilmleri)]
-    kurallar = kurallar.sort_values(by='antecedent support',ascending=False)["antecedents"].apply(lambda x: str(x)).drop_duplicates()
+
+    if antecedent == True:
+        kurallar["antecedents_len"] = kurallar["antecedents"].apply(lambda x: len(x))
+        kurallar = kurallar[kurallar['antecedents_len'] == 1]
+        kurallar = kurallar[kurallar["antecedents"].isin(KategoriFilmleri)]
+        kurallar = kurallar.sort_values(by='antecedent support',ascending=False)
+        kurallar["antecedents"] = kurallar["antecedents"].apply(lambda x: str(x))
+        kurallar = kurallar["antecedents"].drop_duplicates()
+    else:
+        kurallar["consequents_len"] = kurallar["consequents"].apply(lambda x: len(x))
+        kurallar = kurallar[kurallar['consequents_len'] == 1]
+        kurallar = kurallar[kurallar["consequents"].isin(KategoriFilmleri)]
+        kurallar = kurallar.sort_values(by='lift',ascending=False)
+        kurallar["consequents"] = kurallar["consequents"].apply(lambda x: str(x))
+        kurallar = kurallar["consequents"].drop_duplicates()
     return kurallar
--- Paketler/UygulamaFonk.py
+++ Paketler/UygulamaFonk.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import Paketler.Kategori as kat
 import Paketler.Apriori as ap
+import Paketler.Dosyalar as dosya
 
 def Iddenİsme_Çoğul(idler):
-    filmlerCSV = "C:/Users/gunes/Desktop/odevprogramlama/yazılımgeliştirme/filmönermeuygulaması/HazırVeriler/filmler.csv"
-    filmler = pd.read_csv(filmlerCSV, usecols=["movieId","title"])
+    filmler = pd.read_csv(dosya.filmlerCSV, usecols=["movieId","title"])
     
     idler = idler.apply(lambda x: int(str(x).replace("{","").replace("}","").replace("'","")))
     idler = idler.rename("id")
@@ -21,11 +21,18 @@ def Iddenİsme_Çoğul(idler):
     return isimler
 
 def Iddenİsme_Tekil(id):
-    filmlerCSV = "C:/Users/gunes/Desktop/odevprogramlama/yazılımgeliştirme/filmönermeuygulaması/HazırVeriler/filmler.csv"
-    filmler = pd.read_csv(filmlerCSV, usecols=["movieId","title"])
+    filmler = pd.read_csv(dosya.filmlerCSV, usecols=["movieId","title"])
     
     isim = filmler[filmler["movieId"].apply(lambda x: str(x).__eq__(id))]
     return isim
+
+def SetDahil(kullanıcıFilmleri, FilmIDSet):
+    dahil = True
+    FilmIDSet = FilmIDSet.split()
+    for film in FilmIDSet:
+        if not kullanıcıFilmleri.__contains__(film):
+            dahil = False
+    return dahil
 
 def BirKur_İsim(id, kurallar): #birliktelik
     kurallar = kurallar[kurallar['antecedents'] == {id}]
@@ -34,6 +41,18 @@ def BirKur_İsim(id, kurallar): #birliktelik
     return öneriler
 
 def BirKur_Tür(kategori, kurallar):
-    filmler = kat.KategorilerinPopülerleri(kurallar, kategori)
+    filmler = kat.KategorilerinPopülerleri(kurallar, kategori, True)
+    öneriler = Iddenİsme_Çoğul(filmler)
+    return öneriler
+
+def Kişisel_Tür(kullanıcı, kurallar, kategori):
+    kullanıcılar = pd.read_csv(dosya.kullanıcılarCSV)
+    kullanıcınınFilmleri = kullanıcılar["movieId"].iloc[kullanıcı-1]
+
+    kurallar['antecedents'] = [str(i).replace("{","").replace("}","").replace(" ","").replace("'","") for i in kurallar['antecedents']]
+    kurallar = kurallar[kurallar['antecedents'].apply(lambda x: SetDahil(kullanıcınınFilmleri, x))]
+    filmler = kat.KategorilerinPopülerleri(kurallar, kategori, False)
+    dönüştürülmüşFilmler = filmler.apply(lambda x: str(x).replace("{","").replace("}","").replace("'",""))
+    filmler = filmler[~(dönüştürülmüşFilmler.apply(lambda x: SetDahil(kullanıcınınFilmleri, x)))]
     öneriler = Iddenİsme_Çoğul(filmler)
     return öneriler
